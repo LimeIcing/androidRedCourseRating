ae9a12c904a8ff30bb0826db9ed098044fd2327b
Lime Icing <emil577b@stud.kea.dk>
2019-03-26 14:12:41 +0100

    Made login layout and started login button functionality

---
+++ app/src/main/java/com/example/redcourserating/CourseListActivity.java
@@ -0,0 +1,19 @@
+package com.example.redcourserating;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_list);
+
+        init();
+    }
+
+    private void init() {
+
+    }
+}
---
+++ app/src/main/java/com/example/redcourserating/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.redcourserating;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG = "MainActivity";
+    private EditText fieldUsername, fieldPassword;
+    private Button btnLogIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == btnLogIn.getId()) {
+            Log.d(TAG, "Button press registered");
+
+            if (fieldUsername.getText().toString().isEmpty() ||
+                    fieldPassword.getText().toString().isEmpty()) {
+                Toast.makeText(this, "Please fill out both fields", Toast.LENGTH_SHORT);
+            } else {
+                Intent intent = new Intent(this, CourseListActivity.class);
+                startActivity(intent);
+            }
+        }
+    }
+
+    private void init() {
+        fieldUsername = findViewById(R.id.fieldUsername);
+        fieldPassword = findViewById(R.id.fieldPassword);
+        btnLogIn = findViewById(R.id.btnLogIn);
+    }
+}
